9c3c94f6ea0ab2c1834c92d3f54d22d394fe9e92
Awaluddin0123 <101578499+Awaluddin0123@users.noreply.github.com>
2022-04-19 15:04:06 +0700

    Add files via upload

---
+++ private attribute.py
@@ -0,0 +1,12 @@
+class Product():
+ __items = 4
+
+def __init__(self, nama_barang):
+    self.nama = nama_barang
+
+def harga_barang(self,harga_barang):
+    hasil = self.__items * harga_barang
+    return hasil
+
+baju = Product("baju")
+print(sepatu.harga_barang(100))
